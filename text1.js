60a08af442df20a3e01c4f506df2b566b96c2e30
muhammedsajid <muhammedsajid@q2kg.onmicrosoft.com>
2023-08-23 11:53:42 +0530

    first

---
+++ text1.js
@@ -0,0 +1,38 @@
+// Named funciton
+// function sample(a,b){
+//     console.log(a+b);
+// }
+// sample(10,20)
+
+
+// Anonymous Fucntons
+
+// console.log(function(a,b){
+//     return a+b
+// }(30,50));
+
+// const greet = function(a,b){
+//     console.log(a+b);
+// }
+// greet(30,50)
+
+
+
+
+
+// (function(){
+//     console.log(`it's a anonymous functioins`);
+//     console.log(person.name);
+// })()
+
+
+
+
+
+
+// Function expression
+
+const sum = function(a,b){
+    return a+b;
+}
+console.log(sum(10,20));
